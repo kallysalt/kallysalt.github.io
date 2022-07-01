0da4c7f0a1164a71e5266a9d3e628a564fdc93e2
Zhubo Zhou <zhubozho@gmail.com>
2022-07-01 10:47:24 -0700

    created a react-based card game

---
+++ magic-match/src/App.js
@@ -0,0 +1,94 @@
+import './App.css'
+import {useState, useEffect} from 'react'
+import SingleCard from './components/SingleCard'
+
+const cardImages = [
+  { "src": "/img/helmet-1.png", matched: false},
+  { "src": "/img/potion-1.png", matched: false},
+  { "src": "/img/ring-1.png", matched: false},
+  { "src": "/img/scroll-1.png", matched: false},
+  { "src": "/img/shield-1.png", matched: false},
+  { "src": "/img/sword-1.png", matched: false},
+]
+
+function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  // generate cards
+  const generateCards = () => {
+    const generateCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5).map((card) => ({...card, id: Math.random()}))
+    
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(generateCards)
+    setTurns(0)
+  }
+
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (cards.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+  
+  // reset choices and increment turn:
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  // start new game automagically
+  useEffect(() => {
+    generateCards()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={generateCards}>New Game</button>
+      <div className="card-grid"> 
+      {cards.map(card => (
+        <SingleCard 
+          key={card.id} 
+          card={card}
+          handleChoice={handleChoice}
+          flipped={card === choiceOne || card === choiceTwo || card.matched}
+          disabled={disabled}
+        />
+      ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  )
+}
+
+export default App
